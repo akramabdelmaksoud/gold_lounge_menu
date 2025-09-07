f3c4f99ae0354d4cc7163ff8e549aac3105f3d92
akramabdelmaksoud <114350400+akramabdelmaksoud@users.noreply.github.com>
2025-09-07 23:57:49 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css"; // ensure Tailwind loads
 
 export const metadata = {
 title: "Gold Lounge — Digital Menu",
-description: "Luxury showcase menu with EN/AR toggle and section heroes.",
+description: "nothin better than being in gold lounge welocme to your second Home ",
   icons: {
     icon: "/logo.jpg",        // path inside /public
     shortcut: "/logo.jpg",
@@ -18,4 +18,5 @@ return (
 <body className="min-h-screen antialiased">{children}</body>
 </html>
 );
+
 }
